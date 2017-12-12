505a7f457a72c5e39eea3b85fb6d0665a70b4688
chen <809783120@qq>
2017-12-12 14:59:50 +0800

    初始代码

---
+++ client.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>WebSocket</title>
+<script src="https://cdn.bootcss.com/jquery/1.12.3/jquery.js"></script>
+<script type="text/javascript">
+	$(function(){
+		var ws = new WebSocket("ws://116.62.58.170:9502");
+		if(ws.readyState!=1){
+    		ws.onopen = function() {
+    			ws.send("ok");
+    			alert("连接成功！");
+    		};
+		}
+		$("#send").click(function(){
+			ws.send(JSON.stringify({fd:1,msg:$(".msg").val()}));
+		});
+		
+		ws.onmessage = function(evt) {
+			var received_msg = evt.data;
+			alert(received_msg);
+		};
+	});
+
+</script>
+</head>
+<body>
+	<div id="sse">
+		<input type="text" class="msg" value=""><button id="send">发送</button>
+	</div>
+</body>
+</html>
---
+++ websocket_server.php
@@ -0,0 +1,23 @@
+<?php
+$server = new swoole_websocket_server("0.0.0.0", 9502);
+$redis = new Redis();
+
+$server->on('open', function ($server, $req) use($redis) {
+    $redis->set("fd_".$req->fd,$req->fd);
+    echo "connection open: {$req->fd}\n";
+});
+
+$server->on('message', function ($server, $frame) {
+    echo "received message: {$frame->data}\n";
+    $data = json_decode($frame->data,true);
+    if ($data['fd']){
+        $server->push($data['fd'], $data['msg']);
+    }
+});
+
+$server->on('close', function ($server, $fd) use($redis) {
+    echo "connection close: {$fd}\n";
+    $redis->del("fd_".$fd);
+});
+
+$server->start();
